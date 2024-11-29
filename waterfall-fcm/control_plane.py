a23e13eb5613dc70facd812dd10190c0e3246530
Joep Boumans <joep_boumans@live.nl>
2024-11-29 11:00:22 +0100

    Set to onie home folder

--- waterfall-fcm/control_plane.py
+++ waterfall-fcm/control_plane.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 import os, sys  
 
-sys.path.append('../../../sde/bf-sde-9.11.0/install/lib/python3.8/site-packages/tofino/')
-sys.path.append('../../../sde/bf-sde-9.11.0/install/lib/python3.8/site-packages/tofino/bfrt_grpc/')
+sys.path.append('/home/onies/de/bf-sde-9.11.0/install/lib/python3.8/site-packages/tofino/')
+sys.path.append('/home/onie/sde/bf-sde-9.11.0/install/lib/python3.8/site-packages/tofino/bfrt_grpc/')
 
 os.environ["GRPC_ENABLE_FORK_SUPPORT"] = "1"
 os.environ["GRPC_POLL_STRATEGY"] = "poll"
