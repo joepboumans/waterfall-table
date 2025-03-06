27755cfb42bb0446ca60911cfb2deecafece8d8f
Joep Boumans <joep_boumans@live.nl>
2025-03-06 10:39:29 +0100

    Disable setting queues

--- waterfall-fcm-srctuple/setup_switch.py
+++ waterfall-fcm-srctuple/setup_switch.py
@@ -91,10 +91,10 @@ clear_all()
 # bfrt.tf1.tm.port.sched_shaping.mod(dev_port=3, unit='PPS', provisioning='MIN_ERROR', max_rate=1, max_burst_size=1)
 # bfrt.tf1.tm.port.sched_shaping.mod(dev_port=5, unit='PPS', provisioning='MIN_ERROR', max_rate=1, max_burst_size=1)
 
-for port_number in [132, 140, 148, 156]:
-    for queue_id in range(8):
-        pipe_num, pg_id, pg_queue=get_pg_info(port_number, queue_id)
-        bfrt.tf1.tm.queue.sched_cfg.mod(pipe=pipe_num, pg_id=pg_id, pg_queue=pg_queue, min_priority=queue_id,max_priority=queue_id)
+# for port_number in [132, 140, 148, 156]:
+#     for queue_id in range(8):
+#         pipe_num, pg_id, pg_queue=get_pg_info(port_number, queue_id)
+#         bfrt.tf1.tm.queue.sched_cfg.mod(pipe=pipe_num, pg_id=pg_id, pg_queue=pg_queue, min_priority=queue_id,max_priority=queue_id)
 
 print("populating forward table...")
 forward_tbl = p4.WaterfallIngress.forward
