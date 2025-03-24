99d0ef2214a074e9bd8f9c702b6c5bd65781f66d
Joep Boumans <joep_boumans@live.nl>
2025-03-24 15:15:44 +0100

    Add better arg parsing

--- waterfall-fcm-srctuple/control_plane/main.cpp
+++ waterfall-fcm-srctuple/control_plane/main.cpp
@@ -8,15 +8,31 @@
 #include <dvm/bf_drv_intf.h>
 #include <getopt.h>
 #include <target-utils/clish/thread.h>
+#include <getopt.h>
 #include <unistd.h>
 
+
 int main(int argc, char *argv[]) {
+  option longopts[] = {{"dataset", required_argument, NULL, 'd'},
+                       {"real", optional_argument, NULL, 'r'}};
   bool real = false;
-  if (argc > 1) {
-    std::cout << "Running for real hardware" << std::endl;
-    real = true;
-  }
   std::string filePath = "/workspace/PDS-Simulator/data/32_test.pcap";
+  while (1) {
+    const int opt = getopt_long(argc, argv, "d:r:", longopts, 0);
+    if (opt == -1) {
+      break;
+    }
+
+    switch(opt) {
+      case 'd':
+        filePath = optarg;
+        std::cout << "Input dataset: " << filePath << std::endl;
+      case 'r':
+        real = true;
+        std::cout << "Set to run on real hardware" << std::endl;
+    } 
+  }
+
   printf("Loading in data set\n");
   pcapReader dataReader(filePath, TupleSize::SrcTuple);
 
