648d1e2bf50c26e9191f6e367cdf6ed4b34703e1
Joep Boumans <joep_boumans@live.nl>
2025-03-27 09:46:03 +0100

    Exit after having received more than 2 packets

--- waterfall/control_plane/waterfall.cpp
+++ waterfall/control_plane/waterfall.cpp
@@ -200,7 +200,7 @@ void Waterfall::run() {
     if (std::chrono::duration_cast<std::chrono::milliseconds>(time -
                                                               lastReceivedTime)
                 .count() >= 5000 and
-        hasReceivedFirstDigest) {
+        ControlPlane::mLearnInterface.mLearnDataVec.size() > 1) {
       std::cout << "Have no received any digest for over 1s, quiting run loop"
                 << std::endl;
 
