bad2f001e5647f0d326d970f86066f14c97117f4
Joep Boumans <joep_boumans@live.nl>
2025-03-27 10:51:56 +0100

    Add printing to digest

--- waterfall/control_plane/ControlPlane.cpp
+++ waterfall/control_plane/ControlPlane.cpp
@@ -24,12 +24,23 @@ handleLearnCallback(const bf_rt_target_t &bf_rt_tgt,
 
   learnInterface *cpLearnInterface = (learnInterface *)cookie;
 
+  std::cout << "Got " << learnDataVec.size() << " digest packets which are: ";
   for (auto &data : learnDataVec) {
     uint64_t val_hi;
     data->getValue(1, &val_hi);
     uint64_t val_lo;
     data->getValue(2, &val_lo);
     uint64_t val = (val_hi << 16) + val_lo;
+    vector<uint8_t> src_addr(4);
+    memcpy(src_addr.data(), &val, 4);
+    std::reverse(src_addr.begin(), src_addr.end());
+    for (auto &x : src_addr) {
+      std::cout << int(x);
+      if (&x != &src_addr.back()) {
+        std::cout << ".";
+      }
+    }
+    std::cout << "\t";
     cpLearnInterface->mLearnDataVec.push_back(val);
     /*data->getValue(2, &val);*/
     /*cpLearnInterface->mLearnDataVec.push_back(val);*/
